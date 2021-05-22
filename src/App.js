4df054505d985abaa08cfdc332879f1c7e2f6f18
Suhas Bhairav <suhasbhairav@gmail.com>
2021-05-22 11:46:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useCallback, useMemo} from 'react';
 import './App.css';
 
 function square(number){
-
   return number * number;
 };
 
 function App() {
   const [counter, setCounter] = useState(1);
-  const res = square(counter);
+  const res = useMemo(() => {
+    return square(counter);
+  }, [counter]);
+
+  const [name, setName] = useState('');
+
+
+  const displayName = useCallback(
+    () => {
+      return name
+    },
+    [name],
+  );
+
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
@@ -16,9 +28,31 @@ function App() {
       <button onClick={(e) => setCounter(counter + 1)}>Increment</button>
       <button onClick={(e) => setCounter(counter - 1)}>Decrement</button>
       
+      <div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />        
+      </div>
+      <div>
+        <JustTyped displayName={displayName} />
+      </div>
       
     </div>
   );
 }
+
+
+const JustTyped = ({displayName}) => {
+  const [value, setValue] = useState("");
+  
+  useEffect(() => {
+    setValue(displayName());
+    console.log("Calling useEffect...");
+  }, [displayName]);
+
+  return (
+    <div>
+      Just typed: {value}
+    </div>
+  )
+};
 
 export default App;
