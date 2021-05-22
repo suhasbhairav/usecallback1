a78e61eb9e2558a1dcfd12767fee555aec0c230f
Suhas Bhairav <suhasbhairav@gmail.com>
2021-05-22 11:22:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useCallback} from 'react';
 import './App.css';
 
+function square(number){
+
+  return number * number;
+};
+
 function App() {
+  const [counter, setCounter] = useState(1);
+  const res = square(counter);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {counter}</h1>
+      <h2>Square of {counter} is {res}</h2>
+      <button onClick={(e) => setCounter(counter + 1)}>Increment</button>
+      <button onClick={(e) => setCounter(counter - 1)}>Decrement</button>
+      
+      
     </div>
   );
 }
